c87d383ac64fa39a78676181cde02cfa8d7b0a34
Areeb Mohammed <areebmohammed11@gmail.com>
2021-05-16 10:36:42 -0500

    Added battery info publishing

--- ASTRA Prototype Testing/src/main.cpp
+++ ASTRA Prototype Testing/src/main.cpp
@@ -46,6 +46,52 @@ u_int32_t lastGPSTime = 0;
 
 void publishPVTdata(UBX_NAV_PVT_data_t);
 
+//*******************************
+// Motor Drivers Configuration
+//*******************************
+int numMotors = 8;
+int motorPWMPins[] = {23, 4, 6, 24, 25, 28, 29, 33};
+int motorDIRPins[] = {22, 5, 7, 26, 27, 30, 31, 32};
+
+int frleftMotorIdx = 0;
+int frrightMotorIdx = 1;
+int bkleftMotorIdx = 7;
+int bkrightMotorIdx = 2;
+
+boolean frleftMotorRev = true;
+boolean frrightMotorRev = false;
+boolean bkleftMotorRev = true;
+boolean bkrightMotorRev = false;
+
+int frleftMotorSpd = 0;
+int frrightMotorSpd = 0;
+int bkleftMotorSpd = 0;
+int bkrightMotorSpd = 0;
+
+int motorSpeed = 0;
+int motorDir = 1;
+
+//*******************************
+// Battery Voltage Reader Config
+//*******************************
+#define BAT_SENS_PIN PIN_A2
+ADC *adc = new ADC();              // adc object
+float cells = 4;                   // Number of battery cells present
+float minBatVoltage = 3.4 * cells; // Below this voltage motors should refuse to function
+float maxBatVoltage = 4.2 * cells; // Battery voltage that should be considered fully charged
+
+float batteryVoltage;
+float batteryCharge;
+
+u_int32_t lastBatteryTime = 0;
+void publishBatteryData();
+
+//*******************************
+// Command Handling
+//*******************************
+void parseCommand(String command);
+void setMotors(float magnitude, float direction);
+
 // Sensor Reading Values
 // Config Variables
 
@@ -94,19 +140,55 @@ void setup()
     }
 
     // Prime Motors
+    // Set motor pins to output
+    for (int i = 0; i < numMotors; i++)
+    {
+        pinMode(motorPWMPins[i], OUTPUT);
+        pinMode(motorDIRPins[i], OUTPUT);
+    }  
     // Setup Battery Voltage Measurement
+    // Set battery voltage sense pin to input
+    pinMode(BAT_SENS_PIN, INPUT);
+
+    // Configure ADC0
+    adc->adc0->setAveraging(16);                                         // set number of averages
+    adc->adc0->setResolution(16);                                        // set bits of resolution
+    adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED); // change the conversion speed
+    adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED);          // change the sampling speed
+
+    //Read battery voltage
+    int value1 = adc->adc0->analogRead(BAT_SENS_PIN); // get raw ADC reading
+    float batVoltage = value1 * 3.3 / (1.0 / (1.0 + 10.0)) / adc->adc0->getMaxValue(); // convert reading to voltage
+    float batCharge = (batVoltage - minBatVoltage) / (maxBatVoltage - minBatVoltage); // convert voltage to naive charge percent (could be improved by factoring in discharge curves)
 
     clear_strip(clear_color);
 }
 
 void loop()
 {
+    //Read battery voltage and charge
+    int value1 = adc->adc0->analogRead(BAT_SENS_PIN);
+    batteryVoltage = value1 * 3.3 / (1.0 / (1.0 + 10.0)) / adc->adc0->getMaxValue();
+    batteryCharge = (batteryVoltage - minBatVoltage) / (maxBatVoltage - minBatVoltage);
+
     // Process GPS
-    if (millis() - 250 > lastGPSTime && GNSS_enable)
+    if (millis() - lastGPSTime > 250 && GNSS_enable)
     {
         lastGPSTime = millis();
         myGNSS.checkUblox();     // Check for the arrival of new data and process it.
         myGNSS.checkCallbacks(); // Check if any callbacks are waiting to be processed.
+    }
+
+    if (millis() - lastBatteryTime > 1000)
+    {
+        lastBatteryTime = millis();
+        publishBatteryData();
+    }
+
+    if (Serial.available())
+    {
+        String command = Serial.readStringUntil('\n');
+        parseCommand(command);
     }
     // Two Jobs
     //  - Relay Sensor Data
@@ -116,7 +198,19 @@ void loop()
     //  - Read Commands
     //      - Unpack Commands
     //      - Execute Commands
-    //      - 
+    //      -
+}
+
+void parseCommand(String command)
+{
+    String exec = command.substring(0, command.indexOf(';'));
+    if (exec.equals("set_motors"))
+    {
+        String mag = command.substring(exec.length()+1, command.indexOf(','));
+        String dir = command.substring(command.indexOf(',')+1);
+
+        Serial.println("response;" + exec + " " + mag + " " + dir);
+    }
 }
 
 void clear_strip(uint32_t color)
@@ -179,6 +273,21 @@ void publishPVTdata(UBX_NAV_PVT_data_t ubxDataStruct)
     long horizontal_accuracy = ubxDataStruct.hAcc;
     Serial.print(",horizontal_accuracy=");
     Serial.println(horizontal_accuracy);
+
+    set_single_color(SERIAL_TX_LED, clear_color);
+}
+
+void publishBatteryData()
+{
+    set_single_color(SERIAL_TX_LED, serial_tx_color);
+
+    Serial.print("battery;");
+
+    Serial.print("v=");
+    Serial.print(batteryVoltage);
+
+    Serial.print(",c=");
+    Serial.println(batteryCharge);
 
     set_single_color(SERIAL_TX_LED, clear_color);
 }
